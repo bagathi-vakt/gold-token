a68cc4396100144a08dee6e5b77ec12c00c8b473
bagathi jogarao <bagathijogarao@bagathi-jogaraos-Arm-MacBook-Pro.local>
2022-08-25 14:01:30 +0530

    token factory contract

--- src/components/TokenFactory.js
+++ src/components/TokenFactory.js
@@ -1,55 +1,36 @@
 import React, { useEffect, useState } from "react";
 import { ethers } from "ethers";
-import goldabi from "../contracts/gold_token_abi.json";
-import goldbytecode from "../contracts/gold_bytecode.json";
-var factory;
+import factory_abi from "../contracts/factory_abi.json";
+const contractAddress = "0x93e1Bd666c2670A6576794e656c120ECa2ddDD14";
 function TokenFactory() {
-  const [tokens, setTokens] = useState([]);
   useEffect(() => {
     const fun = async () => {
       const _provider = new ethers.providers.Web3Provider(window.ethereum);
       await _provider.send("eth_requestAccounts", []);
       const signer = _provider.getSigner();
-      factory = new ethers.ContractFactory(goldabi, goldbytecode, signer);
+
+      const contract = new ethers.Contract(
+        contractAddress,
+        factory_abi,
+        signer
+      );
+
+      contract.on("ERC20TokenCreated", (address) => {
+        console.log(`address is ${address}`);
+      });
+      console.log(contract);
+      const tx = await contract.deployNewERC20Token("doge", "dog", 2, 1000);
+      const recipt = await tx.wait();
+
+      console.log(tx);
+      console.log(recipt);
     };
     fun();
   }, []);
-  const handleSubmit = async (e) => {
-    // console.log("e is ", e);
-    e.preventDefault();
-    const name = e.target.name.value;
-    const symbol = e.target.symbol.value;
-
-    const contract = await factory.deploy(
-      e.target.name.value,
-      e.target.symbol.value
-    );
-    const address = contract.address;
-    const token = {
-      address,
-      name,
-      symbol,
-    };
-    setTokens([...tokens, token]);
-  };
 
   return (
     <div>
       <h3>Token Factory </h3>
-      <div>
-        <form onSubmit={handleSubmit}>
-          <input id="name" placeholder="name of token" />
-          <input id="symbol" placeholder="symbol" />
-          <button type="submit">create token</button>
-        </form>
-        <ul>
-          {tokens.map((token) => (
-            <li key={token.address}>
-              {token.symbol}-{token.name}-{token.address}
-            </li>
-          ))}
-        </ul>
-      </div>
     </div>
   );
 }
